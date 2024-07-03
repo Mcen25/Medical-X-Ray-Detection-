23c20be2fc7f8997a49b87384be71956168bf681
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-07-03 19:43:44 -0400

    new imports and read csv

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -1,2 +1,20 @@
+import os
+import zipfile
+import random
 import tensorflow as tf
-print(tf.__version__)
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from shutil import copyfile
+
+import numpy as np
+import pandas as pd
+from glob import glob
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("../input/Data_Entry_2017.csv")
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+    tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
